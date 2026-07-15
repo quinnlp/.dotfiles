3d024143ced422497e1dab169beec4de170ff261
Quinn Pham <quinnlp12@gmail.com>
2026-07-15 18:49:00 +0000

    add /home/jovyan/opt as a place to find nvim

--- profile/.path.sh
+++ profile/.path.sh
@@ -1,5 +1,13 @@
 # Neovim
-if [ -d /opt/nvim-linux-x86_64/bin ]; then
+if [ -d "$HOME/opt/nvim-linux-x86_64/bin" ]; then
+	case ":$PATH:" in
+		*":$HOME/opt/nvim-linux-x86_64/bin:"*)
+			;;
+		*)
+			PATH="$PATH:$HOME/opt/nvim-linux-x86_64/bin"
+			;;
+	esac
+elif [ -d /opt/nvim-linux-x86_64/bin ]; then
 	case ":$PATH:" in
 		*":/opt/nvim-linux-x86_64/bin:"*)
 			;;
